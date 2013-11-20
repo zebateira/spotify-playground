20d270ea72d2e3b08f4fdcebce655a691bbb28c1
Zé Bateira <jlageb+github@gmail.com>
2013-11-20 16:50:33 +0000

    Spotify colors added and "by artist" note added.
    spotify color from guidlines added.
    album and track results now show "by artist" label.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,82 +1,67 @@
 'use strict';
 
+function myFilter(parsedResponse) {
+	var datums = [],
+			type = parsedResponse.info.type + '',
+			list = parsedResponse[type + 's'],
+	max = {
+		artist: 5,
+		album: 5,
+		track: 10
+	}[type];
+
+	// thumbnail_url = https://embed.spotify.com/oembed/?url=
+
+	for (var i = 0; i <= max || i < list.length; i++) {
+		if( list[i] ) {
+			datums.push({
+				value: list[i].name,
+				popularity: list[i].popularity,
+				type: type,
+				href: list[i].href,
+				artist: list[i].artists ? list[i].artists[0].name : list[i].name
+			});
+		}
+	}
+
+	return datums;
+}
+
 $('#ta').typeahead([
 	{
 		name: 'Artists',
 		remote: {
-			url: 'http://ws.spotify.com/search/1/artist.json?q=%QUERY',
-			filter: function(parsedResponse) {
-				var max = 5, datums = [];
-
-				for (var i = 0; i <= max; i++) {
-					if( parsedResponse.artists[i] ) {
-						datums.push({
-							value: parsedResponse.artists[i].name,
-							name: parsedResponse.artists[i].name,
-							popularity: parsedResponse.artists[i].popularity,
-							type: parsedResponse.artists[i].type,
-							href: parsedResponse.artists[i].href
-						});
-					}
-				}
-
-				return datums;
-			}
+			url: '//ws.spotify.com/search/1/artist.json?q=%QUERY',
+			filter: myFilter
 		},
 		header: '<span class="my-header">Artists</span>'
 	},
   {
 		name: 'Albums',
 		remote: {
-			url: 'http://ws.spotify.com/search/1/album.json?q=%QUERY',
-			filter: function(parsedResponse) {
-				var max = 3, datums = [];
-
-				for (var i = 0; i <= max; i++) {
-					if( parsedResponse.albums[i] ) {
-						datums.push({
-							value: parsedResponse.albums[i].name,
-							name: parsedResponse.albums[i].name,
-							type: parsedResponse.albums[i].type,
-							popularity: parsedResponse.albums[i].popularity,
-							href: parsedResponse.albums[i].href
-						});
-					}
-				}
-				console.log(datums);
-
-				return datums;
-			}
+			url: '//ws.spotify.com/search/1/album.json?q=%QUERY',
+			filter: myFilter
 		},
 		engine: Hogan,
-		template: '<span>{{value}}</span>',
+		template: '<span>{{value}}</span><span class="by-artist">by {{artist}}</span>',
 		header: '<span class="my-header">Albums</span>'
 	},
 	{
 		name: 'Tracks',
 		remote: {
-			url: 'http://ws.spotify.com/search/1/track.json?q=%QUERY',
-			filter: function(parsedResponse) {
-				var max = 10, datums = [];
-
-				for (var i = 0; i <= max; i++) {
-					if( parsedResponse.tracks[i] ) {
-						datums.push({
-							value: parsedResponse.tracks[i].name,
-							name: parsedResponse.tracks[i].name,
-							type: parsedResponse.tracks[i].type,
-							popularity: parsedResponse.tracks[i].popularity,
-							href: parsedResponse.tracks[i].href
-						});
-					}
-				}
-				return datums;
-			}
+			url: '//ws.spotify.com/search/1/track.json?q=%QUERY',
+			filter: myFilter
 		},
+		engine: Hogan,
+		template: '<span>{{value}}</span><span class="by-artist">by {{artist}}</span>',
 		header: '<span class="my-header">Tracks</span>',
 	}
-]).on('typeahead:selected', function(obj, datum) {	
-	$('#play-btn').attr('src', 'https://embed.spotify.com/?uri=' + datum.href + '&view=coverart').show();
+]).on('typeahead:selected', function(obj, datum) {
+	$('#play-btn').attr('src',
+		'//embed.spotify.com/?uri=' +
+		datum.href +
+		(datum.type !== 'album' ? '&view=coverart' : '')
+	).show();
 });
 
 $('.typeahead.input-sm').siblings('input.tt-hint').addClass('hint-small');
